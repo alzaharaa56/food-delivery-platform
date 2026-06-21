b015ae08497e27a5ccec49a18afc15ca360d8ff3
alzahara <shahdaljabri33@gmail.com>
2026-06-21 11:27:19 +0400

    feat: add CustomerService for handling customer registration logic

---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/services/CustomerService.java
@@ -0,0 +1,26 @@
+package com.codeline.food_delivery_platform.services;
+
+import com.codeline.food_delivery_platform.dtos.CustomerRequestDTO;
+import com.codeline.food_delivery_platform.entities.Customer;
+import com.codeline.food_delivery_platform.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Customer registerCustomer(CustomerRequestDTO requestDTO) {
+
+        Customer customer = Customer.builder()
+                .name(requestDTO.getName())
+                .email(requestDTO.getEmail())
+                .phone(requestDTO.getPhone())
+                .status("ACTIVE")
+                .build();
+
+        return customerRepository.save(customer);
+    }
+}
