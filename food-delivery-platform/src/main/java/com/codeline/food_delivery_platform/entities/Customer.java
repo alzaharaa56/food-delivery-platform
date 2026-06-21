5c9241bf754887debda619b44541171984ab47f4
alzahara <shahdaljabri33@gmail.com>
2026-06-21 11:15:34 +0400

    feat: complete day 1 sprint 0 setup and customer entity with repository

---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/entities/Customer.java
@@ -0,0 +1,29 @@
+package com.codeline.food_delivery_platform.entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String status;
+}
---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/repositories/CustomerRepository.java
@@ -0,0 +1,18 @@
+package com.codeline.food_delivery_platform.repositories;
+
+import com.codeline.food_delivery_platform.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+
+    @Query("SELECT c FROM Customer c WHERE c.status = :status")
+    List<Customer> findCustomersByStatus(@Param("status") String status);
+}
+
