591191d5f728dde1a095f53a3d3b31d2aae396ee
alzahara <shahdaljabri33@gmail.com>
2026-06-21 11:23:08 +0400

    feat: translate validation messages in CustomerRequestDTO

---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/dtos/CustomerRequestDTO.java
@@ -0,0 +1,23 @@
+package com.codeline.food_delivery_platform.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerRequestDTO {
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Phone number cannot be blank")
+    @Size(min = 8, max = 15, message = "Phone number must be between 8 and 15 digits")
+    private String phone;
+}
