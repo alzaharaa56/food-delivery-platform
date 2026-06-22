0719c166cfda03349b14ffcaaf64eb7f2106033b
alzahara <shahdaljabri33@gmail.com>
2026-06-22 09:27:06 +0400

    update CustomerRequestDTO

--- food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/Dtos/CustomerRequestDTO.java
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/Dtos/CustomerRequestDTO.java
@@ -1,4 +1,27 @@
 package com.codeline.food_delivery_platform.Dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
 public class CustomerRequestDTO {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+?[0-9]{8,15}$", message = "Invalid phone number format")
+    private String phone;
+
+    @NotBlank(message = "Password is required")
+    private String passwordHash;
 }
