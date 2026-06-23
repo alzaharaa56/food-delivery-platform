b90b472d84d4c03f2b7b329263265ec60f9ded2a
alzahara <shahdaljabri33@gmail.com>
2026-06-23 10:29:46 +0400

    Add  DriverSummaryResponse

---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/DTO/summary/DriverSummaryResponse.java
@@ -0,0 +1,23 @@
+package com.codeline.food_delivery_platform.DTO.summary;
+
+import com.codeline.food_delivery_platform.Entities.DeliveryDriver;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DriverSummaryResponse {
+    private Integer id;
+    private String driverCode;
+    private String fullName;
+    private String phone;
+
+    public static DriverSummaryResponse fromEntity(DeliveryDriver driver) {
+        DriverSummaryResponse summary = new DriverSummaryResponse();
+        summary.setId(driver.getId());
+        summary.setDriverCode(driver.getDriverCode());
+        summary.setFullName(driver.getFirstName() + " " + driver.getLastName());
+        summary.setPhone(driver.getPhone());
+        return summary;
+    }
+}
