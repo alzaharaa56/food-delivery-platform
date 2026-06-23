913c7241dde2d0ae9aab84dbe2d14ce97fcdbbab
alzahara <shahdaljabri33@gmail.com>
2026-06-23 10:04:03 +0400

    Add RestaurantSummaryResponse

---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/DTO/summary/RestaurantSummaryResponse.java
@@ -0,0 +1,23 @@
+package com.codeline.food_delivery_platform.DTO.summary;
+
+import com.codeline.food_delivery_platform.Entities.Restaurant;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RestaurantSummaryResponse {
+    private Integer id;
+    private String name;
+    private String cuisineType;
+    private Boolean acceptingOrders;
+
+    public static RestaurantSummaryResponse fromEntity(Restaurant restaurant) {
+        RestaurantSummaryResponse summary = new RestaurantSummaryResponse();
+        summary.setId(restaurant.getId());
+        summary.setName(restaurant.getName());
+        summary.setCuisineType(restaurant.getCuisineType());
+        summary.setAcceptingOrders(restaurant.getAcceptingOrders());
+        return summary;
+    }
+}
