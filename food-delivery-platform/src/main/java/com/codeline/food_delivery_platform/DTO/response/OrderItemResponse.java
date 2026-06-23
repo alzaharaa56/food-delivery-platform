15a08c7506af02befdf15155bbfab6b10cdc504b
alzahara <shahdaljabri33@gmail.com>
2026-06-23 10:12:24 +0400

    Add  OrderItemResponse

---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/DTO/response/OrderItemResponse.java
@@ -0,0 +1,27 @@
+package com.codeline.food_delivery_platform.DTO.response;
+
+import com.codeline.food_delivery_platform.Entities.OrderItem;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderItemResponse {
+    private Integer id;
+    private String itemName;
+    private Integer quantity;
+    private Double unitPrice;
+    private Double totalPrice;
+    private String specialInstructions;
+
+    public static OrderItemResponse fromEntity(OrderItem item) {
+        OrderItemResponse response = new OrderItemResponse();
+        response.setId(item.getId());
+        response.setItemName(item.getMenuItem().getName());
+        response.setQuantity(item.getQuantity());
+        response.setUnitPrice(item.getUnitPrice());
+        response.setTotalPrice(item.getTotalPrice());
+        response.setSpecialInstructions(item.getSpecialInstructions());
+        return response;
+    }
+}
