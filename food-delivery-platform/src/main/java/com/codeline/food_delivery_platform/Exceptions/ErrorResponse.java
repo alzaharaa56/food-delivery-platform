1a12f2abda2cb7ecf4dda277a7fac07a61e11ca5
alzahara <shahdaljabri33@gmail.com>
2026-06-23 12:14:28 +0400

    add exceptions

---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/Exceptions/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.codeline.food_delivery_platform.Exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    public ErrorResponse(int status, String error, String message, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+}
+
