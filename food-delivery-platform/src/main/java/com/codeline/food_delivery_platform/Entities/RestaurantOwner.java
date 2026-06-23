85957824ea2648f92222bb616c1e5c700c017e69
alzahara <shahdaljabri33@gmail.com>
2026-06-23 08:22:32 +0400

    add RestaurantOwnerRequestDTO

--- food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/Entities/RestaurantOwner.java
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/Entities/RestaurantOwner.java
@@ -2,6 +2,8 @@ package com.codeline.food_delivery_platform.Entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,4 +19,14 @@ public class RestaurantOwner extends BaseUser{
 
     @OneToMany(mappedBy = "restaurantOwner")
     private List<Restaurant> restaurants;
+    private @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+?[0-9]{8,15}$", message = "Phone number must be between 8 and 15 digits") String phone;
+
+    public void setPhone(@NotBlank(message = "Phone number is required") @Pattern(regexp = "^\\+?[0-9]{8,15}$", message = "Phone number must be between 8 and 15 digits") String phone) {
+        this.phone = phone;
+    }
+
+    public @NotBlank(message = "Phone number is required") @Pattern(regexp = "^\\+?[0-9]{8,15}$", message = "Phone number must be between 8 and 15 digits") String getPhone() {
+        return phone;
+    }
 }
