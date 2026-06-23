a0d61a6c1548c7eab6533ff33d31ed215d0804e4
alzahara <shahdaljabri33@gmail.com>
2026-06-23 10:26:02 +0400

    Add  DriverResponse

---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/DTO/response/DriverResponse.java
@@ -0,0 +1,36 @@
+package com.codeline.food_delivery_platform.DTO.response;
+
+import com.codeline.food_delivery_platform.Entities.DeliveryDriver;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DriverResponse {
+    private Integer id;
+    private String driverCode;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String vehicleType;
+    private String vehiclePlate;
+    private Boolean isOnline;
+    private Double currentLat;
+    private Double currentLng;
+
+
+    public static DriverResponse fromEntity(DeliveryDriver driver) {
+        DriverResponse response = new DriverResponse();
+        response.setId(driver.getId());
+        response.setDriverCode(driver.getDriverCode());
+        response.setFullName(driver.getFirstName() + " " + driver.getLastName());
+        response.setEmail(driver.getEmail());
+        response.setPhone(driver.getPhone());
+        response.setVehicleType(driver.getVehicleType());
+        response.setVehiclePlate(driver.getVehiclePlate());
+        response.setIsOnline(driver.getIsOnline());
+        response.setCurrentLat(driver.getCurrentLat());
+        response.setCurrentLng(driver.getCurrentLng());
+        return response;
+    }
+}
