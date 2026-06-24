393b313945b71ddb003eb6cd4a211902918d7fe8
alzahara <shahdaljabri33@gmail.com>
2026-06-24 08:49:30 +0400

    add CustomerController

---
+++ food-delivery-platform/src/main/java/com/codeline/food_delivery_platform/Controllers/CustomerController.java
@@ -0,0 +1,34 @@
+package com.codeline.food_delivery_platform.Controllers;
+
+import com.codeline.food_delivery_platform.dto.request.CustomerRegisterRequest;
+import com.codeline.food_delivery_platform.dto.response.CustomerResponse;
+import com.codeline.food_delivery_platform.dto.summary.CustomerSummaryResponse;
+import com.codeline.food_delivery_platform.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/register")
+    public ResponseEntity<CustomerResponse> registerCustomer(@RequestBody CustomerRegisterRequest request) {
+        return ResponseEntity.ok(customerService.registerCustomer(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> getCustomerById(@PathVariable Integer id) {
+        return ResponseEntity.ok(customerService.getCustomerById(id));
+    }
+
+    @GetMapping("/summary")
+    public ResponseEntity<List<CustomerSummaryResponse>> getAllCustomersSummary() {
+        return ResponseEntity.ok(customerService.getAllCustomersSummary());
+    }
+}
